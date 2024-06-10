36c4e9095a36e97511fbb88a089d999d9fd48f00
vugsk <ganchukovnikita@bk.ru>
2024-06-10 20:12:06 +0700

    гз

--- Assets/scripts/CardMain.cs
+++ Assets/scripts/CardMain.cs
@@ -23,20 +23,20 @@ class InfoCards
         new CardInfo("Водоплавающие",  "Может быть атаковано только хищником со свойством водоплавающие",  "Sprites/card_test"),
         new CardInfo("Быстрое", "Когда атаковано хищником, бросьте кубик. Если 4, 5, 6 - не съедено.", "UI/card"),
         new CardInfo("Мимикрия",  "Когда первый раз за ход атакованно хищником, должно перенаправлено атаку на другое свое животное.",  "UI/card"),
-        new CardInfo("Средний",   "Description  3",   "UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card"),
-        new CardInfo("Средний",   "Description  3",   "Assets/UI/card")
+        new CardInfo("Большой",   "Может быть атаковано только хищникос со свойством большой.",   "UI/card"),
+        new CardInfo("Топотун",   "Может использовать в каждом раунде свою фазу питания - уничтожить одну 'красную' на кормовой базе.",   "Assets/UI/card"),
+        new CardInfo("Ядовитый",   "Хищник съевший это животное в фазу вымирания текущего хода погибает",   "Assets/UI/card"),
+        new CardInfo("Отбрасывание хвоста",   "Когда животное атаковано хищником, поместить в сбррос карту этого или другого имеющегося у животного свойства - остается в живых. Хищник получает одну синию",   "Assets/UI/card"),
+        new CardInfo("Спячка",   "Можно использовать в свою фазу питания - животное считается накормленным. Нельзя использовать два хода подряд и в последнйи ход игры.",   "Assets/UI/card"),
+        new CardInfo("Падальщики",   "Description  3",   "Assets/UI/card"),
+        new CardInfo("Симбиоз",   "Description  3",   "Assets/UI/card"),
+        new CardInfo("Пиратство",   "Description  3",   "Assets/UI/card"),
+        new CardInfo("Камуфляж",   "Description  3",   "Assets/UI/card"),
+        new CardInfo("Норное",   "Description  3",   "Assets/UI/card"),
+        new CardInfo("Сотрудничество",   "Description  3",   "Assets/UI/card"),
+        new CardInfo("Острое зрение",   "Description  3",   "Assets/UI/card"),
+        // new CardInfo("паразит",   "Description  3",   "Assets/UI/card"),
+        // new CardInfo("Средний",   "Description  3",   "Assets/UI/card")
     };
 }
 
@@ -77,9 +77,8 @@ public class CardMain : MonoBehaviour
 {
     public void Awake()
     {
-        foreach (var card in InfoCards.listCard)
-        {
-            ManagerCard.cards.Add(new Card(card));
-        }
+        for (int i = 0; i < 2; i++)
+            foreach (var card in InfoCards.listCard)
+                ManagerCard.cards.Add(new Card(card));
     }
 }
